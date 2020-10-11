f0135d2ec9f5c67d197c55d8163f71d10f722c5a
Greg <gregcudzich@gmail.com>
2020-10-11 12:38:37 -0500

    added refresh of page when user deletes item from list.

--- week7HW_Greg_Cudzich/public/services.js
+++ week7HW_Greg_Cudzich/public/services.js
@@ -20,12 +20,16 @@ $(document).ready(function() {
 
 $(document).on('click', 'button.delete', function(event) {
     var id = event.target.id;
-
+    console.log($(this).parent());
     $.ajax({
         url: "/?id=" + id,
-        type: 'DELETE'
+        type: 'DELETE',
+        success: function () {
+            //only way to remove the item client side in "real-time".
+            //If there was a way to do it by grabbing the list element and removing it I'd like to know.
+            window.location.reload();
+        }
     })
-    //getUpdateItems()
 })
 
 function getAllItems() {
@@ -58,19 +62,3 @@ function getLatestItem() {
         }
     })
 }
-
-// function getUpdateItems() {
-//     $.getJSON({
-//         url: '/list',
-//         success: function(data) {
-//             var list = [];
-//             $.each(data, function(i, item) {
-//                 list.push("<li>" + item.listItem + "<button type='button' class='btn btn-danger btn-sm delete' id='"+item._id+"'>x</button></li>");
-//             })
-//
-//             $("<ul>", {
-//                 html: list.join("")
-//             }).appendTo($('#list'));
-//         }
-//     })
-// }
